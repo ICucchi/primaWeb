6c29d026edfbe51eeba9311dd58541c4bb855fa5
Iacopo Cucchi <116788568+ICucchi@users.noreply.github.com>
2023-12-02 11:07:22 +0100

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace cucchi.iacopo._5i.primaweb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/Prenotazione.cs
@@ -0,0 +1,6 @@
+public class Prenotazione 
+{
+    public string? Nome {get; set; }
+    public string? Email {get; set; }
+    
+}
